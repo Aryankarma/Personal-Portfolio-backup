4bc291ac1563d4a8116a64f7cd21feb9bb963c3c
aryan <aryankarma29@gmail.com>
2023-12-24 13:29:10 +0000

    two desktop pages complete

--- Personal Portfolio/components/TextAnimation.jsx
+++ Personal Portfolio/components/TextAnimation.jsx
@@ -55,4 +55,56 @@ function TextAnimation() {
   );
 }
 
-export default TextAnimation
+// export default TextAnimation
+export default TextAnimation2;
+
+
+function TextAnimation2() {
+  const [part, setPart] = useState("");
+  let i = 0;
+  let offset = 0;
+  let forwards = true;
+  let skipCount = 0;
+  const skipDelay = 15;
+
+  const wordFlick = () => {
+    const animate = () => {
+      if (forwards) {
+        if (offset >= words[i].length) {
+          ++skipCount;
+          if (skipCount === skipDelay) {
+            forwards = false;
+            skipCount = 0;
+          }
+        }
+      } else {
+        if (offset === 0) {
+          forwards = true;
+          i++;
+          offset = 0;
+          if (i >= words.length) {
+            i = 0;
+          }
+        }
+      }
+      const currentPart = words[i].substr(0, offset);
+      if (skipCount === 0) {
+        if (forwards) {
+          offset++;
+        } else {
+          offset--;
+        }
+      }
+      setPart(currentPart);
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  };
+
+  useEffect(() => {
+    wordFlick();
+  }, []); // Run once when the component mounts
+
+  return <div id="subhead">{part}</div>;
+}
--- Personal Portfolio/src/App.jsx
+++ Personal Portfolio/src/App.jsx
@@ -4,26 +4,51 @@ import TextAnimation from "../components/TextAnimation"
 function App() {
   return <>
 
+  <div className="page">
 
-  <div className="nav">
-    <i className="fa-solid fa-moon"></i>
-  </div>
+    <div className="nav">
+      <i className="fa-solid fa-moon"></i>
+    </div>
 
-  <div className="headProfile">
-
-    <div id='headContainer'>
-      <div className="maskContainer">
-        <span id='imaryan' >I'm Aryan Karma</span>
+    <div className="headProfile">
+      <div id='headContainer'>
+        <div className="maskContainer">
+          <span id='imaryan' >I'm Aryan Karma</span>
+        </div>
+        <TextAnimation /> {/* rendering component */}
       </div>
 
-      {/* id subhead is given */}
-      <TextAnimation /> 
+      <div id="profileContainer">
+        <img src="/img/profilePicture.png" alt="profile picture" />
+      </div>
+    </div>
+    
+    <div id='removeMobile' className="hr">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+    
+
+  </div>
+
+  <div className="page">
+
+    <div id='removeMobile' className="head">Bio<span id="dotpurple">.</span></div>
+
+    <div className="contentBox">
+      <p className='content'>
+      “Keen interest in attention to detail and passion for the smallest details makes my work pixel perfect.”
+      </p>
     </div>
 
-    <div id="profileContainer">
-      <img src="/img/profilePicture.png" alt="profile picture" />
+    <div id='removeMobile' className="hr">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
     </div>
-
 
   </div>
 
